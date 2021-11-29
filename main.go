3a52c2a87dc209d8ee9451e5a25b637f0c7b44e9
Harikrushna V Adiecha <adiechahari@gmail.com>
2021-11-29 13:25:31 +0530

    added config

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -13,8 +12,18 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type RepoDetail struct {
+	Name string `json:"name"`
+	Diff int    `json:"max_minutes"`
+}
+
+type RepoList struct {
+	ListRepo []RepoDetail `json:"repos"`
+}
+
 type RunningWorkflow struct {
 	Id        int    `json:"id"`
+	Url       string `json:"url"`
 	Name      string `json:"name"`
 	Status    string `json:"status"`
 	StartedAt string `json:"run_started_at"`
@@ -53,19 +62,37 @@ func restartAction(flow *RunningWorkflow) {
 	// Cancel workflow
 	apiCall("POST", flow.CancelUrl, nil)
 
+	// Wait for work flow to be cancelled grassfully.
+	for {
+		body := apiCall("GET", flow.Url, nil)
+		data := RunningWorkflow{}
+		if err := json.Unmarshal(body, &data); err != nil {
+			panic(err)
+		}
+		if data.Status != "in_progress" {
+			fmt.Println("Workflow " + data.Status + " so moving towards re-run the flow")
+			break
+		}
+		fmt.Println("Workflow still in progress. -- we are waiting.")
+		time.Sleep(2 * time.Second)
+	}
+
 	// ReRun workflow
 	apiCall("POST", flow.ReRunUrl, nil)
+	fmt.Println("Completed.")
 
 }
 
-func ghActionCheck() {
+func ghActionCheck(repo *RepoDetail) {
 
-	body := apiCall("GET", "https://api.github.com/repos/Improwised/apricot-2/actions/runs?status=in_progress", nil)
+	body := apiCall("GET", "https://api.github.com/repos/"+repo.Name+"/actions/runs?status=in_progress", nil)
 
 	fres := RunningActionResponse{}
 	if err := json.Unmarshal(body, &fres); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+
+	fmt.Println("Found " + string(fres.TotalCount) + " Currently running")
 
 	for i, item := range fres.WorkflowRuns {
 		t, e := time.Parse(time.RFC3339, item.StartedAt)
@@ -77,7 +104,8 @@ func ghActionCheck() {
 		diff := time.Now().Sub(t).Minutes()
 
 		fmt.Println(t, diff)
-		if diff > 12 {
+		if diff > float64(repo.Diff) {
+			fmt.Println("Restarting actions.")
 			restartAction(&item)
 		}
 
@@ -87,8 +115,15 @@ func ghActionCheck() {
 
 func main() {
 	godotenv.Load()
-	ghActionCheck()
-	// for {
-	// 	time.Sleep(1 * time.Minute)
-	// }
+	file, _ := ioutil.ReadFile("conf.json")
+	data := RepoList{}
+	json.Unmarshal([]byte(file), &data)
+
+	for {
+		for _, repo := range data.ListRepo {
+			fmt.Println(repo)
+			ghActionCheck(&repo)
+		}
+		time.Sleep(1 * time.Minute)
+	}
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,52 @@
 # Github action watcher
 
+Basic objective of this project is to re-run workflow if that stucks at any point of execution. this is temparary solution to keep watching github actions and trigger cancell and rerun as needed.
+
+### Generate token
+
+To run this application you supposed to be generate respective github token (to access private repo on behalf of you)
+
+Use this link to generate token
+[https://github.com/settings/tokens](https://github.com/settings/tokens)
+
+
+### Configure
+
+Basically we need to set the generated token to `.env` file. for that you need to copy `.env.example` file to `.env` file. and add your token and username there
+
+```bash
+cp .env.example .env
+vi .env
+# or you can open it in any of the text editor of your choice
+# and update the values of token and username
 ```
+
+Another configuration that is required is to set repo and it's max time limit to allow github actions to run.
+
+for that you can copy `conf.example.json` to `conf.json`
+
+```bash
+cp conf.example.json conf.json
+```
+
+and make respective changes.
+
+**Name** - `string` Repo name will be combination of owner and name if you take this repo as an example then then name field should have value as `AdiechaHK/gha-watcher`
+
+**Max Minutes** - `int` Maximum time that we allow github actions to be run this will be in integer and that will indecats minutes to allow run and wait.
+
+
+### Execute 
+
+##### for development
+
+```bash
 go run main.go
+```
+
+##### for production
+
+```bash
+go run build
+./gha-watcher
 ```
